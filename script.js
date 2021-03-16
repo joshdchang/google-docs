dbd30ab901cbfb49a2353312337f8390ab193517
Josh Chang <joshchang04@gmail.com>
2021-03-16 15:02:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var href = window.location.href;
 setInterval(() => {
   if(window.location.href !== href){
     href = window.location.href;
-    fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${href}`);
+    fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
   }
 }, 50);
 
